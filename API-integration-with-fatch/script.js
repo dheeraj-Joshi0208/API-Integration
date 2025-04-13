b7b79d1b2fa647408cbcaca76df15e543aa9ae33
dheeraj-Joshi0208 <dheerajjoshi0208@gmail.com>
2025-04-13 17:29:39 +0530

    Create separation.

---
+++ API-integration-with-fatch/script.js
@@ -0,0 +1,20 @@
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    const productDiv = document.querySelector(".card-container");
+    data.products.forEach((product) => {
+      productDiv.innerHTML += `
+       <div class="card"">
+        <img src="${product.images[0]}" class="card-img-top" alt="${product.title}">
+        <div class="card-body">
+          <h5 class="card-title">${product.title}</h5>
+          <p class="card-text">${product.description}</p>
+        </div>
+      </div>
+      `;
+    });
+  })
+  .catch((error) => console.error("Error fetching data:", error));
+
+
